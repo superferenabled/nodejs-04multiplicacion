27a126a436c57014bdaf175132f1bb8068824e04
Fernando Castillo <superferenabled@gmail.com>
2026-09-20 16:49:55 -0600

    Refactor nodemon and package.json configurations for improved development setup; add initial app tests

---
+++ src/app.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest';
+
+describe('App', () => {
+  test('should be true', () => {
+    expect(true).toBe(true);
+    expect(false).toBe(false);
+  });
+});

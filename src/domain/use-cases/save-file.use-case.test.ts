1679fdae3f0524a6474f56c6fb09b4707ba4a7c3
Fernando Castillo <superferenabled@gmail.com>
2026-09-21 22:57:38 -0600

    Refactor save file test cases to improve readability by adding spacing between code blocks

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -13,8 +13,10 @@ describe('savefileusecase', () => {
 
   test('should savefile with default values', () => {
     const saveFile = new SaveFile();
+
     const filePath = './out/table.txt';
     const options = { fileContent: 'test content' };
+
     const result = saveFile.execute(options);
     const fileExists = fs.existsSync(filePath);
     const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
@@ -26,14 +28,17 @@ describe('savefileusecase', () => {
 
   test('should savefile with custom values', () => {
     const saveFile = new SaveFile();
+
     const fileName = 'table-10';
     const destination = 'out/tests';
     const filePath = `./${destination}/${fileName}.txt`;
+
     const options = {
       fileContent: 'test content custom',
       fileName,
       destination,
     } as SaveFileOptions;
+
     const result = saveFile.execute(options);
     const fileExists = fs.existsSync(filePath);
     // console.log({ fileExists, filePath, result });

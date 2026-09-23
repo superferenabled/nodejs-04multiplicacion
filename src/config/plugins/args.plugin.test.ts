625abe00097bdbf48e27ff0b95e36791c95308fc
Fernando Castillo <superferenabled@gmail.com>
2026-09-22 22:08:28 -0600

    Refactor argv tests to use async function and improve structure; add custom value test cases

--- src/config/plugins/args.plugin.test.ts
+++ src/config/plugins/args.plugin.test.ts
@@ -1,8 +1,51 @@
-import { describe, expect, test } from 'vitest';
-import { yarg } from './yargs.plugin';
+import { beforeEach } from 'node:test';
+import { describe, expect, test, vi } from 'vitest';
+
+const originalArgv = process.argv;
+const runCommand = async (args: string[]) => {
+  process.argv = [...originalArgv, ...args];
+  vi.resetModules();
+  const { yarg } = await import('./yargs.plugin');
+  return yarg;
+};
 
 describe('argv tests', () => {
-  test('should return default values', () => {
-    console.log(yarg)
-  })
-})
+  test('should return default values', async () => {
+    const argv = await runCommand(['-b', '5']);
+    // console.log(argv);
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 5,
+        l: 10,
+        s: false,
+        n: 'table',
+        d: './out',
+      }),
+    );
+  });
+
+  test('should return configuration with custom values', async () => {
+    const argv = await runCommand([
+      '-b',
+      '10',
+      '-l',
+      '50',
+      '-s',
+      'true',
+      '-n',
+      'multitable',
+      '-d',
+      'in',
+    ]);
+    // console.log(argv);
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 10,
+        l: 50,
+        s: true,
+        n: 'multitable',
+        d: 'in',
+      }),
+    );
+  });
+});

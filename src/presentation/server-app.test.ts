a1ff50e415f590f1916b25da87ac2123b3169958
Fernando Castillo <superferenabled@gmail.com>
2026-09-23 16:52:21 -0600

    Comment out the server app test case and enhance the custom values test with improved logging and mock implementations

--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -21,24 +21,51 @@ describe('serverapp', () => {
     expect(serverApp).toBeInstanceOf(ServerApp);
     expect(typeof ServerApp.run).toBe('function');
   });
-  test('should run server app with options', () => {
+
+  // test('should run server app with options', () => {
+  //   ServerApp.run(options);
+
+  //   expect(logSpy).toHaveBeenCalledTimes(3);
+  //   expect(logSpy).toHaveBeenCalledWith('Server running...');
+  //   expect(logSpy).toHaveBeenCalledWith('File Created!');
+
+  //   expect(createTableSpy).toHaveBeenCalledTimes(1);
+  //   expect(createTableSpy).toHaveBeenCalledWith({
+  //     base: options.base,
+  //     limit: options.limit,
+  //   });
+
+  //   expect(saveFileSpy).toHaveBeenCalledTimes(1);
+  //   expect(saveFileSpy).toHaveBeenCalledWith({
+  //     fileContent: expect.any(String),
+  //     destination: options.destPath,
+  //     fileName: options.fileName,
+  //   });
+  // });
+
+  test('should run withy custom values mocked', () => {
+    const createMock = vi.fn().mockReturnValue('1 x 2 = 2');
+    const saveFileMock = vi.fn().mockReturnValue(true);
+    const logMock = vi.fn();
+    const logErrorMock = vi.fn();
+
+    console.log = logMock;
+    console.error = logErrorMock;
+    CreateTable.prototype.execute = createMock;
+    SaveFile.prototype.execute = saveFileMock;
+
     ServerApp.run(options);
 
-    expect(logSpy).toHaveBeenCalledTimes(3);
-    expect(logSpy).toHaveBeenCalledWith('Server running...');
-    expect(logSpy).toHaveBeenCalledWith('File Created!');
-
-    expect(createTableSpy).toHaveBeenCalledTimes(1);
-    expect(createTableSpy).toHaveBeenCalledWith({
+    expect(logMock).toHaveBeenCalledWith('Server running...');
+    expect(createMock).toHaveBeenCalledWith({
       base: options.base,
       limit: options.limit,
     });
-
-    expect(saveFileSpy).toHaveBeenCalledTimes(1);
-    expect(saveFileSpy).toHaveBeenCalledWith({
-      fileContent: expect.any(String),
+    expect(saveFileMock).toHaveBeenCalledWith({
+      fileContent: '1 x 2 = 2',
       destination: options.destPath,
       fileName: options.fileName,
     });
+    expect(logMock).toHaveBeenCalledWith('File Created!');
   });
 });

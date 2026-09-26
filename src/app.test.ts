6f39110c6e9957e7a663af22594de92dd1b3b77e
Fernando Castillo <superferenabled@gmail.com>
2026-09-26 14:59:04 -0600

    Implement server run test with mock values and refactor test structure

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,8 +1,32 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
+import { ServerApp } from './presentation/server-app';
 
 describe('App', () => {
-  test('should be true', () => {
-    expect(true).toBe(true);
-    expect(false).toBe(false);
+  test('should call server run with values', async () => {
+    const serverRunMock = vi.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '10',
+      '-l',
+      '5',
+      '-s',
+      'true',
+      '-n',
+      'test-file',
+      '-d',
+      'test-destination',
+    ];
+
+    await import('./app')
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: 'test-file',
+      destPath: 'test-destination'
+    })
   });
 });
